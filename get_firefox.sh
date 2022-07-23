97a96c05590c902940fd1088effe9de3defe0193
dimez <koptev@gmail.com>
2022-07-24 01:42:38 +0300

    Update get_firefox.sh
    Add russian version

--- get_firefox.sh
+++ get_firefox.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# Russian version
+#URL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=ru"
+# English version
 URL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+
 VER=`curl -sfI "$URL" | grep -o 'firefox-[0-9.]\+[0-9]'`
 if [ ! -d "/opt/$VER" ]
 then
