68ec31d44cebe53350062d34d4edba2473eab681
dimez <koptev@gmail.com>
2022-07-24 01:16:12 +0300

    Create get_firefox.sh
    Inifial commit

---
+++ get_firefox.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+URL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+VER=`curl -sfI "$URL" | grep -o 'firefox-[0-9.]\+[0-9]'`
+if [ ! -d "/opt/$VER" ]
+then
+ mkdir /opt/$VER
+ cd /opt
+ curl -sL "$URL" -o $VER.tar.bz2
+ tar xjf $VER.tar.bz2 --strip-components=1 -C $VER
+ ln -sf $VER firefox
+else
+ echo "Latest version already installed"
+fi
