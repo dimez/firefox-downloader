7aaf1e3da1887e303593c75bdcd874a3c04deae1
dimez <koptev@gmail.com>
2022-08-19 02:20:15 +0300

    Fixed bug with new profile after updating
    Hude thanks to krasnh from LOR!

--- get_firefox.sh
+++ get_firefox.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-# Russian version
-#URL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=ru"
-# English version
-URL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+#set -x
 
+URL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
 VER=`curl -sfI "$URL" | grep -o 'firefox-[0-9.]\+[0-9]'`
-if [ ! -d "/opt/$VER" ]
+if [ ! -s "/opt/$VER.tar.bz2" ]
 then
- mkdir /opt/$VER
  cd /opt
+ mv firefox firefox.prev
  curl -sL "$URL" -o $VER.tar.bz2
- tar xjf $VER.tar.bz2 --strip-components=1 -C $VER
- ln -sf $VER firefox
+ tar xjf $VER.tar.bz2
 else
- echo "Latest version already installed"
+ echo "Latest version already downloaded."
 fi
