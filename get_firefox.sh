e2e6a8370d29e6d2d11a88ea31979b88481ad34e
dimez <koptev@gmail.com>
2022-08-19 02:21:59 +0300

    Forgot about Russian version

--- get_firefox.sh
+++ get_firefox.sh
@@ -2,7 +2,11 @@
 
 #set -x
 
+# Russian version
+#URL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=ru"
+# English version
 URL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+
 VER=`curl -sfI "$URL" | grep -o 'firefox-[0-9.]\+[0-9]'`
 if [ ! -s "/opt/$VER.tar.bz2" ]
 then
